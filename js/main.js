09228ad832aee90ec716ea86a9b966d585edf4ae
turneru <49686023+turneru@users.noreply.github.com>
2022-08-29 16:14:06 -0500

    August Updates
    - Animations update
    - Fixed header
    - No sidebar nav
    - Form testing
    - Logo update
    - Smooth scroll links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
- $(document).ready(function() {
+$(document).ready(function() {
  
-     $(window).on('hashchange', function() {
+	$(window).on('hashchange', function() {
          
          if (window.location.hash == "#about") {
             $("#btn2").addClass("active_hover");
@@ -21,33 +21,18 @@
          else {
              $("#btn2, #btn3, #btn4").removeClass("active_hover");
          }
-          })
-     
-
-//    $(".beer1").click( function () {
-//         $(".beer_wrapper").hide( "slide", {direction: "left" }, 300, function() { 
-//         $(".beer_nav").show( "slide", {direction: "up" }, 400 );
-//             $(".beer1_bio").show( "slide", {direction: "right" }, 400 );
-//         });
-//    });
-//   
-//    $(".back").click( function () {
-//         $(".beer1_bio, .beer2_bio, .beer3_bio, .beer4_bio, .beer5_bio, .beer6_bio").hide( "slide", {direction: "right" }, 300, function() { 
-//         $(".beer_nav").hide( "slide", {direction: "up" }, 400 );
-//         $(".beer_wrapper").delay(300).show( "slide", {direction: "left" }, 400);    
-//         });
-//    }); 
-     
+	});
+      
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
     if (scroll > 25) {
-        $("header").addClass("slide-out-left").removeClass("slide-in-left");
+        //$("header").addClass("slide-out-left").removeClass("slide-in-left");
         $(".side").show().addClass("slide-in-left-fast").removeClass("slide-out-left");
     } 
     
     else {
-        $("header").addClass("slide-in-left").removeClass("slide-out-left");
+        //$("header").addClass("slide-in-left").removeClass("slide-out-left");
         $(".side").hide().addClass("slide-out-left").removeClass("slide-in-left-fast"); 
     }
   
@@ -62,10 +47,10 @@ $(window).scroll(function() {
     } 
 
     else if (scroll >= 1000 && scroll <= 1700 ) {
-        $("#btn2").addClass("active_hover");
-         $("#btn1, #btn3, #btn4").removeClass("active_hover");
-        $(".skill_left").addClass("slide-in-left");
-        $(".skill_right").addClass("slide-in-right");
+//        $("#btn2").addClass("active_hover");
+//        $("#btn1, #btn3, #btn4").removeClass("active_hover");
+//        $(".skill_left").addClass("slide-in-left");
+//        $(".skill_right").addClass("slide-in-right");
     } 
     
     else if (scroll >= 1701 && scroll <= 2600) {
